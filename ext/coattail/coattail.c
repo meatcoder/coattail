bf7bdec0a76fba847d2dd7f12cabe53ef0d18415
Sim Domingo <sim@github.com>
2017-08-15 23:38:20 +0800

    simplify interface

--- ext/coattail/coattail.c
+++ ext/coattail/coattail.c
@@ -10,22 +10,23 @@ static int open_file(VALUE obj) {
 
     //actually open file
     int fd = open(fname, O_RDONLY);
-    if (fd == -1) rb_raise(rb_eRuntimeError, "Unable to open file: %s (%s)", fname, strerror(errno));
+    if (fd == -1) rb_raise(rb_eRuntimeError, "Unable to open file: %s (%s).", fname, strerror(errno));
 
     return fd;
 }
 
-//static void get_last_lines(int fd, int num_lines)
+static VALUE each_line(VALUE self) {
+    //look for block param
+    if (!rb_block_given_p()) rb_raise(rb_eArgError, "Block argument is required.");
 
-static VALUE each_line_ext(VALUE self, VALUE num_lines, VALUE wait_indefinitely, VALUE blk) {
+    //open file
     int fd = open_file(self);
     if (fd < 0) return Qnil;
 
-    //if (NUM2INT(num_lines) != 0)
     return Qnil;
 }
 
 void Init_coattail(void) {
     VALUE cCoatTail = rb_const_get(rb_cObject, rb_intern("CoatTail"));
-    rb_define_method(cCoatTail, "each_line_ext", each_line_ext, 3);
+    rb_define_method(cCoatTail, "each_line", each_line, 0);
 }
