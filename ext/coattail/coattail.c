77d70eebd47093926f701c7a2378669c9055d33f
Sim Domingo <sim@github.com>
2017-08-16 11:32:47 +0800

    implemented main loop logic

--- ext/coattail/coattail.c
+++ ext/coattail/coattail.c
@@ -2,6 +2,11 @@
 #include <fcntl.h>
 #include <string.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+//sleep interval in microseconds
+#define SLEEP_INTERVAL 250000
 
 static int open_file(VALUE obj) {
     //get filename from object
@@ -15,6 +20,10 @@ static int open_file(VALUE obj) {
     return fd;
 }
 
+static void read_lines(int fd, off_t* pos) {
+
+}
+
 static VALUE each_line(VALUE self) {
     //look for block param
     if (!rb_block_given_p()) rb_raise(rb_eArgError, "Block argument is required.");
@@ -23,7 +32,22 @@ static VALUE each_line(VALUE self) {
     int fd = open_file(self);
     if (fd < 0) return Qnil;
 
-    return Qnil;
+    //seek to end of file
+    off_t cur_pos = lseek(fd, 0, SEEK_END);
+    if (cur_pos == -1) rb_raise(rb_eRuntimeError, "Unable seek to end of file (%s).", strerror(errno));
+
+    while (1) {
+        //get file size
+        struct stat info;
+        int res = fstat(fd, &info);
+        if (res == -1) rb_raise(rb_eRuntimeError, "Unable get size of file (%s).", strerror(errno));
+
+        //check if the size of the file changed from earlier
+        if (info.st_size > cur_pos) read_lines(fd, &cur_pos);
+        else usleep(SLEEP_INTERVAL);
+    }
+
+    return Qtrue;
 }
 
 void Init_coattail(void) {
--- ext/coattail/extconf.rb
+++ ext/coattail/extconf.rb
@@ -3,5 +3,7 @@ require "mkmf"
 abort "no fcntl.h" unless  have_header "fcntl.h"
 abort "no string.h" unless  have_header "string.h"
 abort "no errno.h" unless  have_header "errno.h"
+abort "no unistd.h" unless  have_header "unistd.h"
+abort "no sys/stat.h" unless  have_header "sys/stat.h"
 
 create_makefile "coattail/coattail"
